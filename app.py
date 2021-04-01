11519facaf054a2c9786cbd621e479458e18ad73
Vaibhav Kumar <vaibhav.k0509@gmail.com>
2021-04-02 00:02:54 +0530

    FastAPI and Streamlit Frontend Added

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
+import uvicorn
+from pydantic import BaseModel
 from Prediction import *
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-from flask import Flask, request
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/result",methods = ['POST'])
-def result():
-    extract_json = request.get_json(silent=True,force=True)
+class query(BaseModel):
+    txt: str
 
-    """
-    quotes in a list
-    """
-    quotes  = extract_json.get("quotes")
 
-    pred = predict(quotes)
+@app.post("/result")
+def result(line:query):
+    return predict(line.txt)
 
-    return pred
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ =="__main__":
+    uvicorn.run(app,host = "127.0.0.1",port =9000)
---
+++ stream_app.py
@@ -0,0 +1,29 @@
+import yfinance as yf
+import streamlit as st
+
+import requests
+def send_req(x):
+    url = 'http://127.0.0.1:9000/result'
+    myobj = {"txt":x}
+
+    x = requests.post(url, json = myobj)
+
+    return x.text
+
+
+
+st.write("""
+# Text aspect extraction app
+""")
+
+
+st.write("""
+## Enter Text Here
+""")
+
+txt = st.text_input("Enter Your Text","")
+
+
+if st.button("Submit"):
+    result = send_req(txt)
+    st.success(result)
